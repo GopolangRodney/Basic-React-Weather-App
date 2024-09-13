00e4cbfaa25872b84323a0525445f1853ee02ff0
AmiGrae <info.amarigrae@gmail.com>
2024-09-13 14:50:47 +0200

    Resolved merge conflicts

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const footer = () => {
+    return(
+        <>
+        <p><center>copyright 2024. Amari Grae</center></p>
+        <p><center>This Application Contains the Use Of API</center></p>
+        </>
+    );
+}
+
+export default footer;
